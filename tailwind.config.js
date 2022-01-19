253243f3a22f80f9f3256e27ee7c1a12aeeddf4d
Independence <mrolaleyeisrael@gmail.com>
2022-01-20 00:04:16 +0100

    File upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Barlow Semi Condensed, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
